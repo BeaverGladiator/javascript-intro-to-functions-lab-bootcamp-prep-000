5bb7a4e83d11f250c85963e657e91e32806b24af
Matthew Migliore <mmigliore311@gmail.com>
2018-08-16 11:23:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function whisper(string) {
 function logShout(string) {
   var upperCaseString =  `$string`.toUpperCase();
   console.log(upperCaseString)
-  return uppercaseString
+  return upperCaseString
 }
